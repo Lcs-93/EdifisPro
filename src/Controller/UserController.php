14c1935303cadc0cc2cb01b3dc2d344f35fb1fb5
tutez-64 <enzopersillet@gmail.com>
2025-03-04 17:12:36 +0100

    Add User's page navbar buttons and fixed its functioning

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\CompetenceUser;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
@@ -9,31 +10,54 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[Route(name: 'app_user_index', methods: ['GET'])]
-    public function index(UserRepository $userRepository): Response
-    {
-        return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
-        ]);
-    }
+	#[Route(name: 'app_user_index', methods: ['GET'])]
+	public function index(UserRepository $userRepository): Response
+	{
+		return $this->render('user/index.html.twig', [
+			'users' => $userRepository->findAll(),
+		]);
+	}
+
+	#[Route('/user_list', name: 'app_user_list', methods: ['GET'])]
+	public function list(UserRepository $userRepository): Response
+	{
+		return $this->render('user/list.html.twig', [
+			'users' => $userRepository->findAll(),
+		]);
+	}
 
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+	        $competences = $form->get('competences')->getData();
+
+	        $hashedPassword = $passwordHasher->hashPassword($user, $form->get('plainPassword')->getData());
+	        $user->setPassword($hashedPassword);
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+	        foreach ($competences as $competence) {
+		        $competenceUser = new CompetenceUser();
+		        $competenceUser->setUtilisateur($user);
+		        $competenceUser->setCompetence($competence);
+		        $entityManager->persist($competenceUser);
+	        }
+
+	        $entityManager->flush();
+
+
+	        return $this->redirectToRoute('app_user_list', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/new.html.twig', [
@@ -51,15 +75,30 @@ final class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+	        foreach ($user->getCompetenceUsers() as $competenceUser) {
+		        $entityManager->remove($competenceUser);
+	        }
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+	        $competences = $form->get('competences')->getData();
+	        $hashedPassword = $passwordHasher->hashPassword($user, $form->get('plainPassword')->getData());
+	        $user->setPassword($hashedPassword);
+
+	        foreach ($competences as $competence) {
+		        $competenceUser = new CompetenceUser();
+		        $competenceUser->setUtilisateur($user);
+		        $competenceUser->setCompetence($competence);
+		        $entityManager->persist($competenceUser);
+	        }
+
+	        $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_list', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
@@ -72,10 +111,13 @@ final class UserController extends AbstractController
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+	        foreach ($user->getCompetenceUsers() as $competenceUser) {
+		        $entityManager->remove($competenceUser);
+	        }
             $entityManager->remove($user);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_user_list', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -2,12 +2,20 @@
 
 namespace App\Form;
 
+use App\Entity\Competence;
+use App\Entity\CompetenceUser;
 use App\Entity\Role;
 use App\Entity\User;
+use PhpParser\Node\Expr\Array_;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
@@ -17,15 +25,62 @@ class UserType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('email')
-            ->add('password')
-            ->add('role', EntityType::class, [
-                'class' => Role::class,
-                'choice_label' => 'nom',
-	            'placeholder' => "Choisir un role",
-	            'required' => false,
-            ])
-        ;
+	        ->add('plainPassword', PasswordType::class, [
+		        'mapped' => false,
+		        'constraints' => [
+			        new NotBlank([
+				        'message' => 'Veuillez entrer un mot de passe',
+			        ]),
+		        ],
+	        ])
+	        ->add('roles', ChoiceType::class, [
+		        'choices' => [
+			        'Administrateur' => 'ROLE_ADMIN',
+			        'Utilisateur' => 'ROLE_USER',
+		        ],
+		        'expanded' => true,
+		        'multiple' => true,
+	        ])
+	        ->add('competences', EntityType::class, [
+		        'class' => Competence::class,
+		        'choice_label' => 'nomCompetence',
+		        'multiple' => true,
+		        'expanded' => true,
+		        'mapped' => false, // Géré manuellement dans le contrôleur
+		        'required' => false,
+	        ]);
+
+	    // 🔥 Ajouter un écouteur d'événement pour pré-remplir le champ competences
+	    $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+		    $user = $event->getData();
+		    $form = $event->getForm();
+
+		    if (!$user || null === $user->getId()) {
+			    return;
+		    }
+
+		    // Récupérer les compétences associées via CompetenceUser
+		    $competences = [];
+		    foreach ($user->getCompetenceUsers() as $competenceUser) {
+			    $competences[] = $competenceUser->getCompetence();
+		    }
+
+		    // 🔍 Debug : Vérifier les compétences récupérées
+		    dump($competences);
+
+		    // 🔥 Forcer la mise à jour du champ competences
+		    $form->add('competences', EntityType::class, [
+			    'class' => Competence::class,
+			    'choice_label' => 'nomCompetence',
+			    'multiple' => true,
+			    'expanded' => true,
+			    'mapped' => false,
+			    'required' => false,
+			    'data' => $competences, // 🔥 Ajout direct des compétences sélectionnées
+		    ]);
+	    });
     }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
