b187e20edbe194eddf4c811e379855e31af47803
tutez-64 <enzopersillet@gmail.com>
2025-03-05 11:43:11 +0100

    Show User's name and skills after login

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -20,9 +20,7 @@ final class UserController extends AbstractController
 	#[Route(name: 'app_user_index', methods: ['GET'])]
 	public function index(UserRepository $userRepository): Response
 	{
-		return $this->render('user/index.html.twig', [
-			'users' => $userRepository->findAll(),
-		]);
+		return $this->render('user/index.html.twig');
 	}
 
 	#[Route('/user_list', name: 'app_user_list', methods: ['GET'])]
